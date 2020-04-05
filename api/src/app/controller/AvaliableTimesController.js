a99f5dc55bf7cf4389f7d7beebc5d5957d0ed118
Gabriel Guizardi <gabrielguicar55@gmail.com>
2020-04-05 16:17:02 -0300

    add restrição de cadastramento de horários

--- api/src/app/controller/AvaliableTimesController.js
+++ api/src/app/controller/AvaliableTimesController.js
@@ -5,6 +5,17 @@ import { addHours, format } from 'date-fns';
 class AvaliableTimeController {
   async store(req, res) {
     const { avaliable_hours } = req.body;
+    const { volunteer_id } = req.params;
+
+    const volunteerAvaliable = await AvaliableTime.findOne({
+      where: {
+        volunteer_id,
+      },
+    });
+
+    if (volunteerAvaliable !== null) {
+      return res.status(400).json({ error: 'Horas já cadastradas!' });
+    }
 
     const hours = avaliable_hours.map(hour => {
       return format(addHours(parseInt(hour) * 60 * 60 * 1000, 6), 'H:mm');
@@ -12,7 +23,7 @@ class AvaliableTimeController {
 
     const times = await AvaliableTime.create({
       avaliable_hours: hours,
-      volunteer_id: req.params.volunteer_id,
+      volunteer_id,
     }).catch(error => {
       return res.status(500).json({
         message: 'Erro! Entre em contato conosco',
