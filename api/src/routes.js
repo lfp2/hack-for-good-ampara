911ea2c9ebca67cdc2d054411da606a08c950e43
lfp2 <lfp2@cin.ufpe.br>
2020-06-01 12:12:03 -0300

    Adding health professional sign up and login

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 
+const HealthProfessionalController = require("./app/controllers/HealthProfessionalController");
 const VolunteerController = require('./app/controllers/VolunteerController')
 const VolunteerVerifyController = require('./app/controllers/VolunteerVerifyController')
 
@@ -8,6 +9,10 @@ const routes = new Router()
 routes.post('/volunteer', VolunteerController.store)
 
 routes.post('/volunteer/signIn', VolunteerController.login)
+
+routes.post("/healthprofessional", HealthProfessionalController.store);
+
+routes.post("/healthprofessional/signIn", HealthProfessionalController.login)
 
 routes.get('/verify/volunteer', VolunteerVerifyController.index)
 
