dfcdaa9c8542fc658b390eb0a14de664b1277b8e
Yuri <yuri.gabrielaragaobol@gmail.com>
2020-06-04 14:22:58 -0300

    Adding moment.js

--- app/src/pages/volunteer/Calendar.js
+++ app/src/pages/volunteer/Calendar.js
@@ -1,8 +1,4 @@
-import React, { useState }from 'react';
-
-import { ScreenCenter } from '../../assets/styles';
-
-import { useNavigation} from '@react-navigation/native';  
+import React, {useState} from 'react';
 
 import {
     MyScreen,
@@ -13,121 +9,97 @@ import {
     CalendarView,
     ButtonView,
     BackButton,
-    SecondaryText
-
-} from '../../assets/styles/calendar'
+    SecondaryText,
+} from '../../assets/styles/calendar';
 
 import CalendarStrip from 'react-native-calendar-strip';
 import HorizontalDatePicker from '@logisticinfotech/react-native-horizontal-date-picker';
-import * as moment from 'moment'; 
+import moment from 'moment';
 import 'moment/locale/pt-br';
 
-import { Button, FlatList } from 'react-native';
+import {FlatList, Button} from 'react-native';
 
+import {useNavigation} from '@react-navigation/native';
 
-export default function Calendar(){
+export default function Calendar() {
     const navigation = useNavigation();
 
-    function navigateBack(){
+    function navigateBack() {
         navigation.goBack();
     }
 
-    moment.locale('pt-br')
+    moment.locale('pt-br');
 
     // minDate = today
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var yyyy = today.getFullYear();
 
-    today = yyyy + '-' + mm + '-' + dd;
+    const today = moment(new Date()).add(1, 'd').format('MM/DD/YYYY'); //.format('L');
 
-    // Defining maxDate in the calendar - 1 mês a partir de hoje 
-    var maxDate = new Date();
+    console.log(today);
 
-    var mm = String(maxDate.getMonth() + 2).padStart(2, '0');
+    // Defining maxDate in the calendar - 1 mês a partir de hoje
+    const maxDate = moment(new Date()).add(1, 'M');
 
-    maxDate = yyyy + '-' + mm + '-' + dd;
+    //var mm = String(maxDate.getMonth() + 2).padStart(2, '0');
+
+    //maxDate = yyyy + '-' + mm + '-' + dd;
     const [timeSelected, setTime] = useState('');
     const [dateSelected, setDate] = useState(today);
 
-    var array = [{dateSelected, timeSelected}]
-
-    // Conversão para nosso formato de horário 
-    onTimeSelected = time => {
-        var hours = Number(time.match(/^(\d+)/)[1]);
-        var minutes = Number(time.match(/:(\d+)/)[1]);
-        var AMPM = time.match(/\s(.*)$/)[1];
-        if(AMPM == "pm" && hours<12) hours = hours+12;
-        if(AMPM == "am" && hours==12) hours = hours-12;
-        var sHours = hours.toString();
-        var sMinutes = minutes.toString();
-        if(hours<10) sHours = "0" + sHours;
-        if(minutes<10) sMinutes = "0" + sMinutes;
-        time = sHours + ':' + sMinutes;
-        setTime(time)
-    }
+    // Conversão para nosso formato de horário
+    const onTimeSelected = (time) => {
+        setTime(time);
+    };
     // Conversão para nosso formato de data
-    onDateSelected = date => {
-        date = new Date(date)
-        let year = date.getFullYear();
-        let month = (1 + date.getMonth()).toString().padStart(2, '0');
-        let day = date.getDate().toString().padStart(2, '0');
-    
-        date = day + '-' + month + '-' + year;
-        setDate(date)
-    }
+    const onDateSelected = (date) => {
+        date = moment(new Date()).format('DD/MM/YYYY');
+        setDate(date);
+    };
 
     function handleBook() {
-
-        console.log(timeSelected, dateSelected)
-
+        console.log(timeSelected, dateSelected);
     }
-        
+
     return (
-
         <MyScreen>
-
-            <RectangleBackground/>
+            <RectangleBackground />
             <HeaderView>
                 <BackButton>
-                    <HeaderIcon onPress={navigateBack} name = "arrow-left"/>
+                    <HeaderIcon onPress={navigateBack} name="arrow-left" />
                 </BackButton>
                 <TextHeader>Meus horários</TextHeader>
             </HeaderView>
             <CalendarView>
                 <CalendarStrip
-                    scrollable
+                    scrollable={true}
                     useIsoWeekday={false}
                     selectedDate={today}
                     minDate={today}
                     maxDate={maxDate}
                     onDateSelected={onDateSelected}
-                    style={{height:100, paddingBottom: 10}}
+                    style={{height: 100, paddingBottom: 10}}
                     calendarHeaderContainerStyle={{backgroundColor: '#76b1e6'}}
                     calendarHeaderStyle={{color: 'white', fontSize: 20}}
                     dateNumberStyle={{color: 'black', fontSize: 18}}
                     dateNameStyle={{color: 'black', fontSize: 11}}
                     highlightDateNameStyle={{color: '#76b1e6', fontSize: 11}}
                     highlightDateNumberStyle={{color: '#76b1e6', fontSize: 18}}
-                    iconContainer={{flex: 0.1}}            
+                    iconContainer={{flex: 0.1}}
                 />
             </CalendarView>
-            <HorizontalDatePicker 
-                timePickerContainer = {{backgroundColor: '#76b1e6'}}
-                onTimeSelected = {onTimeSelected}
+            <HorizontalDatePicker
+                timePickerContainer={{backgroundColor: '#76b1e6'}}
+                onTimeSelected={onTimeSelected}
                 pickerType={'time'}
                 selectedTextStyle={{color: '#76b1e6'}}
             />
 
-        <ButtonView>
-            <Button
-                onPress={handleBook}
-                title="Disponibilizar"
-                color="#79e7e1"
-            />
-        </ButtonView>
-
+            <ButtonView>
+                <Button
+                    onPress={handleBook}
+                    title="Disponibilizar"
+                    color="#79e7e1"
+                />
+            </ButtonView>
         </MyScreen>
     );
 }
