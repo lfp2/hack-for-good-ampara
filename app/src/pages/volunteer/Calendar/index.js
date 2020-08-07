6b075ea1d1890a404b17d370214c9c2c6e3dc024
Lavínia Paganini <laviniapaganini@gmail.com>
2020-08-07 11:01:42 -0300

    :bug: fix/volunteer calendar issues

--- app/src/pages/volunteer/Calendar/index.js
+++ app/src/pages/volunteer/Calendar/index.js
@@ -13,6 +13,7 @@ import api from 'src/services/api';
 import DataCard from 'src/components/DataCard';
 import useToggle from 'react-use/lib/useToggle';
 import Modal from 'src/components/Modal';
+import { useStoreState } from 'easy-peasy';
 
 export default function CalendarScreen() {
   moment.locale('pt-br');
@@ -31,6 +32,17 @@ export default function CalendarScreen() {
 
   const [isDateAlreadyTaken, toggleIsDateAlreadyTaken] = useToggle(false);
   const [modalVisibility, toggleModalVisibility] = useToggle(false);
+  const {
+    displayName,
+    documentNumber,
+    documentName,
+    email,
+    phoneNumber,
+    token,
+    bio,
+    uf,
+    city,
+  } = useStoreState((state) => state.volunteer);
 
   useEffect(() => {
     const timestamp = moment
@@ -62,11 +74,7 @@ export default function CalendarScreen() {
 
   async function retrieveAgenda() {
     try {
-      const value = await AsyncStorage.getItem('@AmparaApp:volunteer');
-
-      const { token } = JSON.parse(value);
-
-      const response = await api.post('/volunteer/retrieveAvailableHours', {
+      const response = await api.post('/available_hours/list', {
         token,
       });
 
@@ -88,19 +96,7 @@ export default function CalendarScreen() {
         deviceTimezone,
       );
 
-      const value = await AsyncStorage.getItem('@AmparaApp:volunteer');
-
-      const {
-        displayName,
-        documentNumber,
-        documentName,
-        email,
-        phoneNumber,
-        token,
-        bio,
-      } = JSON.parse(value);
-
-      const response = await api.post('/volunteer/makeAvailableHours', {
+      await api.post('/available_hours', {
         token,
         displayName,
         bio,
@@ -109,11 +105,13 @@ export default function CalendarScreen() {
         timestamp,
         email,
         phoneNumber,
+        uf,
+        city,
       });
 
       setUploadTimestamp(timestamp);
     } catch (error) {
-      // console.log(error);
+      console.log(error);
     }
   }
 
